b22c107a5d06d4de80e732fbf2ba024a62a79f5e
Andrew Graham-Yooll <andrewgy8@gmail.com>
2016-07-07 08:59:08 -0400

    Guess feedback

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,33 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-random_int = rand(101)
+random_int = rand(100)
 
 get '/' do
-  erb :index, :locals => {:random_int => random_int}
+  def guess_message(guess, random_int)
+    difference = (random_int - guess).abs
+    
+    if guess > random_int  
+      if difference > 5
+        'Way too high'
+      else
+        'Too high'
+      end
+    elsif guess < random_int
+      if difference > 5
+        'Way too low'
+      else
+        'Too low'
+      end
+    elsif guess == random_int
+      'Right on point'
+    end
+  end
+
+  guess = params['guess'].to_i
+
+  message = guess_message(guess, random_int)
+
+  erb :index, :locals => {:random_int => random_int, :message => message}
 end
 
