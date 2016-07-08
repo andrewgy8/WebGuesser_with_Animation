377e9d72f8292b963ddb611f865d64c4d7f7f34c
Andrew Graham-Yooll <andrewgy8@gmail.com>
2016-07-08 06:45:18 -0400

    Some basic CSS formatting

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,35 +3,33 @@ require 'sinatra/reloader'
 
 
 random_int = rand(100)
+def difference(guess, random_int)
+  difference = (random_int - guess).abs 
+end
+
+def guess_message(guess, random_int, difference)
+  
+  if guess > random_int  
+    if difference > 5
+      'Way too high'
+    else
+      'Too high'
+    end
+  elsif guess < random_int
+    if difference > 5
+      'Way too low'
+    else
+      'Too low'
+    end
+  elsif guess == random_int
+    'Right on point'
+  end
+end
 
 get '/' do
-  def difference(guess, random_int)
-    difference = (random_int - guess).abs 
-  end
-  
-  def guess_message(guess, random_int, difference)
-    
-    if guess > random_int  
-      if difference > 5
-        'Way too high'
-      else
-        'Too high'
-      end
-    elsif guess < random_int
-      if difference > 5
-        'Way too low'
-      else
-        'Too low'
-      end
-    elsif guess == random_int
-      'Right on point'
-    end
-  end
-
   guess = params['guess'].to_i
   difference = difference(guess, random_int)
   message = guess_message(guess, random_int, difference)
-
   erb :index, :locals => {:random_int => random_int, 
                           :message => message, 
                           :difference => difference
